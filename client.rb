b6dc923d6841ff79bb774287265cc531b830272a
Doug Barth <doug@pagerduty.com>
2013-11-25 11:15:51 -0800

    Options parsing for output interval

--- client.rb
+++ client.rb
@@ -5,13 +5,18 @@ require 'optparse'
 Thread.abort_on_exception = true
 
 options = {
-  :concurrency => 1
+  :concurrency => 1,
+  :output_interval => 1
 }
 OptionParser.new do |opts|
   opts.banner = "Usage: ruby client.rb [options] [server[:port]]"
 
   opts.on("-P", "--parallel n", "Number of parallel client connections (default: 1)") do |n|
     options[:concurrency] = n.to_i
+  end
+
+  opts.on("-i", "--interval n", "Output connection rate every n seconds (default: 1)") do |n|
+    options[:output_interval] = n.to_i
   end
 
   opts.on("-h", "--help", "Print usage") do
@@ -24,9 +29,6 @@ server_host, server_port = (ARGV[0] || '').split(':')
 server_port = (server_port || 8080).to_i
 
 connection_rate = (ARGV[1] || 100).to_i
-
-output_interval = 1
-next_output_time = Time.now + output_interval
 
 total_cxns = 0
 connections = 0
@@ -67,11 +69,11 @@ class Timer
 end
 
 timers = options[:concurrency].times.map {|i| Timer.new(connection_rate, "connection source #{i}") }
-output_timer = Timer.new(1/output_interval.to_f, "output")
+output_timer = Timer.new(1/options[:output_interval].to_f, "output")
 
 Thread.new do
   output_timer.each_tick do
-    puts "#{Time.now} - Connections/s = #{connections / output_interval}, Total connections = #{total_cxns}"
+    puts "#{Time.now} - Connections/s = #{connections / options[:output_interval]}, Total connections = #{total_cxns}"
     counter_mutex.synchronize do
       connections = 0
     end
