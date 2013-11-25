75fd3ba41a9037ee485679d2a28cf2171b269fc4
Doug Barth <doug@pagerduty.com>
2013-11-25 11:12:56 -0800

    Real options parsing
    So far --help and --parallel options are added.

--- client.rb
+++ client.rb
@@ -1,13 +1,29 @@
 require 'socket'
 require 'thread'
+require 'optparse'
 
 Thread.abort_on_exception = true
+
+options = {
+  :concurrency => 1
+}
+OptionParser.new do |opts|
+  opts.banner = "Usage: ruby client.rb [options] [server[:port]]"
+
+  opts.on("-P", "--parallel n", "Number of parallel client connections (default: 1)") do |n|
+    options[:concurrency] = n.to_i
+  end
+
+  opts.on("-h", "--help", "Print usage") do
+    puts opts
+    exit
+  end
+end.parse!
 
 server_host, server_port = (ARGV[0] || '').split(':')
 server_port = (server_port || 8080).to_i
 
 connection_rate = (ARGV[1] || 100).to_i
-concurrency = (ARGV[2] || 1).to_i
 
 output_interval = 1
 next_output_time = Time.now + output_interval
@@ -50,7 +66,7 @@ class Timer
   end
 end
 
-timers = concurrency.times.map {|i| Timer.new(connection_rate, "connection source #{i}") }
+timers = options[:concurrency].times.map {|i| Timer.new(connection_rate, "connection source #{i}") }
 output_timer = Timer.new(1/output_interval.to_f, "output")
 
 Thread.new do
